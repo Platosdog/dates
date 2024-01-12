83b09d120cae441fe3e2f53b5c097f3370ca19ed
Platosdog <62915572+Platosdog@users.noreply.github.com>
2024-01-12 10:01:09 +0900

    page-not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DateSelectorComponent } from './date-selector/date-selector.component';
 import { ContainerDisplayComponent } from './container-display/container-display.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  //{ path: '**', component: PageNotFoundComponent },
+  { path: '**', component: PageNotFoundComponent },
   { path: 'dates', component: DateSelectorComponent},
   { path: 'planner', component: ContainerDisplayComponent}
 ];
