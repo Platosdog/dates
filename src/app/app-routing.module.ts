be27a0e86cf8f389a4f49ecf9983982efe99f85a
Platosdog <62915572+Platosdog@users.noreply.github.com>
2024-01-12 09:55:17 +0900

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DateSelectorComponent } from './date-selector/date-selector.component';
+import { ContainerDisplayComponent } from './container-display/container-display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '**', component: PageNotFoundComponent },
+  { path: 'dates', component: DateSelectorComponent},
+  { path: 'planner', component: ContainerDisplayComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
